55bc0480597499b5347a979beabbba6f0af24bae
Idrwla <ilyarredmi@gmail.com>
2020-12-10 20:00:59 +0600

    Created components

---
+++ src/app/active-products/active-products.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-active-products',
+  templateUrl: './active-products.component.html',
+  styleUrls: ['./active-products.component.css']
+})
+export class ActiveProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { SingInComponent } from './sing-in/sing-in.component';
+import { SingUpComponent } from './sing-up/sing-up.component';
+import { GreeterComponent } from './greeter/greeter.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { TicketDetailComponent } from './ticket-detail/ticket-detail.component';
+import { PayPageComponent } from './pay-page/pay-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PartnerPageComponent } from './partner-page/partner-page.component';
+import { ActiveProductsComponent } from './active-products/active-products.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDetailComponent,
+    SingInComponent,
+    SingUpComponent,
+    GreeterComponent,
+    TicketsComponent,
+    TicketDetailComponent,
+    PayPageComponent,
+    NotFoundComponent,
+    PartnerPageComponent,
+    ActiveProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
